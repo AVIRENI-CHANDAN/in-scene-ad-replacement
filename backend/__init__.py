d87008ad6a3b5b50cd076de28a855aaae2379b5c
Avireni Chandan <avireni.chandanchandu43657@gmail.com>
2024-11-15 23:42:02 -0500

    Project database models added to the application

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,8 @@
 from .application import create_app, run_application
 from .cognito_util import sign_up, verify_sign_up
 from .environ import get_environment_variable, parse_bool
-from .routes import register_cognito_auth_endpoints, register_react_base
+from .models import Annotation, Project, Video, create_db_models, initialize_db
+from .routes import (
+    register_cognito_auth_endpoints,
+    register_react_base,
+)
---
+++ backend/models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from .environ import get_environment_variable
+
+db = SQLAlchemy()
+
+
+def initialize_db(app):
+    print("Initialising the database")
+    app.config["SQLALCHEMY_DATABASE_URI"] = get_environment_variable(
+        "SQLALCHEMY_DATABASE_URI"
+    )
+    db.init_app(app)
+
+
+def create_db_models():
+    db.create_all()
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey("project.id"), nullable=False)
+    filename = db.Column(db.String(100))
+
+
+class Annotation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey("project.id"), nullable=False)
+    timestamp = db.Column(db.Float)
+    points = db.Column(db.JSON)
+    image_url = db.Column(db.String(200))
